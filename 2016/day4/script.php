97a4061877badd3dc72c8dbfc6bae2009f77ca8b
Paul Sonnenschein <paulsonnenschein0@gmail.com>
2016-12-05 11:26:08 +0100

    add day4 part2

--- 2016/day4/script.php
+++ 2016/day4/script.php
@@ -14,7 +14,7 @@ $rows = array_map(function ($row) {
   preg_match('/([a-z]+(?:-[a-z]+)*?)-(\d+)\[([a-z]+)\]/', $row, $matches);
   $rowStr = str_replace('-', '', $matches[1]);
 
-  return [sortStr($rowStr), $matches[2], $matches[3]];
+  return [$matches[1], sortStr($rowStr), $matches[2], $matches[3]];
 }, explode("\n", $inputString));
 
 function isValid($str, $checksum) {
@@ -52,9 +52,38 @@ function isValid($str, $checksum) {
 }
 
 $filtered = array_filter($rows, function ($row) {
-  return isValid($row[0], $row[2]);
+  return isValid($row[1], $row[3]);
 });
 
-$result = array_sum(array_column($filtered, 1));
+$result = array_sum(array_column($filtered, 2));
 
 var_dump('part1:', $result);
+
+function str_rot($s, $n = 13) {
+    static $letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $n = (int)$n % 26;
+    if (!$n) return $s;
+    if ($n == 13) return str_rot13($s);
+    for ($i = 0, $l = strlen($s); $i < $l; $i++) {
+        $c = $s[$i];
+        if ($c >= 'a' && $c <= 'z') {
+            $s[$i] = $letters[(ord($c) - 71 + $n) % 26];
+        } else if ($c >= 'A' && $c <= 'Z') {
+            $s[$i] = $letters[(ord($c) - 39 + $n) % 26 + 26];
+        }
+    }
+    return $s;
+}
+
+function decrypt($str, $id) {
+  return str_rot(str_replace('-', ' ', $str), $id % 26);
+}
+
+
+foreach ($filtered as $row) {
+  $decrypted = decrypt($row[0], (int)$row[2]);
+  if ($decrypted === 'northpole object storage') {
+    var_dump('part2:', $row[2]);
+    break;
+  }
+}
